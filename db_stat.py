fa6cbfc99bdb75235c77235568efce437e23ea79
Greeshma <184537@ust-global.com>
2021-09-20 00:02:11 +0530

    Database Status

---
+++ db_stat.py
@@ -0,0 +1,44 @@
+import mysql.connector as mysql
+from rich import console
+from rich.console import Console
+
+console = Console()
+
+db = mysql.connect(
+  host='localhost',
+  user='root',
+  password='root'
+)
+
+cur = db.cursor()
+cur.execute('SHOW STATUS')
+res = cur.fetchall()
+r = dict(res)
+
+
+def get_info_of_db():
+	console.print(f"Uptime, {r['Uptime']}\nThreads_created, {r['Threads_created']}\nThreads_connected, {r['Threads_connected']}\nThreads_running, {r['Threads_running']}\nQueries, {r['Queries']}\nMax_used_connections, {r['Max_used_connections']}", style='bold blue')
+	
+def get_process_list():
+	cur.execute("select ID,DB from PROCESSLIST") 
+	res = cur.fetchall()
+	console.print(res)
+
+
+	
+def menu():
+	console.print("1. Information Of Database\n2. Show Process List\n3. Exit",style="bold cyan")
+	
+	
+
+while True:
+    menu()
+    ch = int(input("Enter the Choice: "))
+    if ch == 1:
+        get_info_of_db()
+    elif ch == 2:
+        get_process_list()
+    else:
+        break
+
+cur.close()
